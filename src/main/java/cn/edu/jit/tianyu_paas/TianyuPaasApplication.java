c7d7c22b49f46ea31913605771f1b5ba04258a76
tianyu <tianyu@tianyu-studio.cn>
2018-06-26 11:24:52 +0800

    init commit

---
+++ src/main/java/cn/edu/jit/tianyu_paas/TianyuPaasApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.jit.tianyu_paas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TianyuPaasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TianyuPaasApplication.class, args);
+    }
+}
