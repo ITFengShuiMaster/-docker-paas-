309a2aa2314436bd987be50f6f46f3a03487135b
luyue <wxhzq520@sina.com>
2018-06-28 17:49:40 +0800

    增加登录注册

---
+++ shared/src/main/java/cn/edu/jit/tianyu_paas/shared/entity/util/RegexUtil.java
@@ -0,0 +1,24 @@
+package cn.edu.jit.tianyu_paas.shared.entity.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author 卢越
+ * @since 2018-06-28
+ */
+public class RegexUtil {
+
+    /**
+     * 正则判断字符串是否是11位数字
+     *
+     * @param num
+     * @return
+     */
+    public static boolean regexElevenNumber(String num) {
+        String regEx = "^(\\d{11})$";
+        Pattern pattern = Pattern.compile(regEx);
+        Matcher matcher = pattern.matcher(num);
+        return matcher.matches();
+    }
+}
