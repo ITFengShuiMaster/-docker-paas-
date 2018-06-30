a65f302bb2f4b09bb2e1d872c16a2a269d4e621a
tianyu <tianyu@tianyu-studio.cn>
2018-06-30 16:45:10 +0800

    优化代码

--- web/src/main/java/cn/edu/jit/tianyu_paas/web/controller/ActionController.java
+++ web/src/main/java/cn/edu/jit/tianyu_paas/web/controller/ActionController.java
@@ -2,7 +2,6 @@ package cn.edu.jit.tianyu_paas.web.controller;
 
 import cn.edu.jit.tianyu_paas.shared.entity.Action;
 import cn.edu.jit.tianyu_paas.shared.enums.ActionEnum;
-import cn.edu.jit.tianyu_paas.shared.util.DateUtil;
 import cn.edu.jit.tianyu_paas.shared.util.TResult;
 import cn.edu.jit.tianyu_paas.web.global.Constants;
 import cn.edu.jit.tianyu_paas.web.service.ActionService;
@@ -14,10 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/actions")
@@ -40,20 +36,11 @@ public class ActionController {
     @GetMapping
     public TResult info() {
         List<Action> lists = actionService.selectList(new EntityWrapper<Action>().eq("user_id", session.getAttribute(Constants.SESSION_KEY_USER_ID)).orderBy("gmt_create", false).last("LIMIT 6"));
-        List<Map<String, Object>> actions = new ArrayList<>();
-        for (Action action : lists) {
-            Map<String, Object> map = new HashMap<>(16);
-            map.put("userId", action.getUserId());
-            map.put("userName", action.getUserName());
-            map.put("action", ActionEnum.getMessageBycode(action.getAction()));
-            map.put("appId", action.getAppId());
-            map.put("appName", action.getAppName());
-            map.put("status", Action.STATUS.equals(action.getStatus()) ? "完成" : "失败");
-            map.put("date", DateUtil.getSimpleDate(action.getGmtCreate()));
-            actions.add(map);
-        }
 
-        return TResult.success(actions);
+        // 添加action的行为名字
+        lists.forEach(action -> action.setActionName(ActionEnum.getMessageBycode(action.getAction())));
+
+        return TResult.success(lists);
     }
 
     /**
