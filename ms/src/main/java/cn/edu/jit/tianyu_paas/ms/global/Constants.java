a1488bd07d2ca909899fbf6d47d6ef0f397dfec8
nilongkang <1104475626@qq.com>
2018-07-02 16:18:33 +0800

    修改ms

--- ms/src/main/java/cn/edu/jit/tianyu_paas/ms/global/Constants.java
+++ ms/src/main/java/cn/edu/jit/tianyu_paas/ms/global/Constants.java
@@ -4,6 +4,4 @@ public class Constants {
 
     public static final String SESSION_KEY_ADMIN_ID = "admin";
     public static final String TOKEN = "TOKEN";
-
-    public static final String SESSION_KEY_USER_ID = "user";
 }
