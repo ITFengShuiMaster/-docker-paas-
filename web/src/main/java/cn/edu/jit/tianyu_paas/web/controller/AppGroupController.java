1e8d65d49146faa84a0f2e89a0ee1e969f3ccbc2
zhangwanping <1774735953@qq.com>
2018-07-02 15:23:07 +0800

    update

--- web/src/main/java/cn/edu/jit/tianyu_paas/web/controller/AppGroupController.java
+++ web/src/main/java/cn/edu/jit/tianyu_paas/web/controller/AppGroupController.java
@@ -38,11 +38,10 @@ public class AppGroupController {
 
     /**
      * 创建应用组
-     *
+     * @author 倪龙康
      * @param groupName
      * @param compose
      * @return
-     * @author 倪龙康
      */
     @PostMapping
     public TResult groupCreate(String groupName, String compose) {
@@ -57,23 +56,32 @@ public class AppGroupController {
         if (!appGroupService.insert(appGroup)) {
             return TResult.failure(TResultCode.FAILURE);
         }
-        return TResult.success(appGroup.getAppGroupId());
+        return TResult.success( appGroup.getAppGroupId() );
     }
 
     /**
      * 修改组名
-     *
+     * @author 倪龙康
      * @param appGroup
      * @return
-     * @author 倪龙康
      */
     @PutMapping
-    public TResult updateGroup(AppGroup appGroup) {
+    public TResult updateGroup(AppGroup appGroup){
         Long userId = (Long) session.getAttribute(Constants.SESSION_KEY_USER_ID);
-        if (!appGroupService.update(appGroup, new EntityWrapper<AppGroup>(
+        if(!appGroupService.update(appGroup, new EntityWrapper<AppGroup>(
         ).eq("app_id", appGroup.getAppGroupId()).and().eq("user_id", userId)))
             return TResult.failure(TResultCode.FAILURE);
         return TResult.success();
+    }
+
+    /**
+     * 获取所有组的信息
+     * @return
+     */
+    @GetMapping
+    public TResult listGruopsInfo() {
+        Long userId = (Long) session.getAttribute(Constants.SESSION_KEY_USER_ID);
+        return TResult.success(appGroupService.selectList(new EntityWrapper<AppGroup>().eq("user_id", userId)));
     }
 
     /**
@@ -82,29 +90,27 @@ public class AppGroupController {
      * @return
      * @author 倪龙康
      */
-    @GetMapping
-    public TResult showGroupInfo() {
+    @GetMapping("groups-apps")
+    public TResult showGroupAndAppInfo() {
         Long userId = (Long) session.getAttribute(Constants.SESSION_KEY_USER_ID);
-        Map<AppGroup, List<App>> maps = new HashMap<>();
         List<AppGroup> groups = appGroupService.selectList(new EntityWrapper<AppGroup>().eq("user_id", userId));
-        for (AppGroup group : groups) {
+        for(AppGroup group: groups) {
             List<App> apps = appService.selectList(new EntityWrapper<App>().eq("app_group_id", group.getAppGroupId()));
-            maps.put(group, apps);
+            group.setApps(apps);
         }
-        return TResult.success(maps);
+        return TResult.success(groups);
     }
 
     /**
      * 删除应用组
-     *
+     * @author 倪龙康
      * @param appGroupId
      * @return
-     * @author 倪龙康
      */
     @DeleteMapping("{appGroupId}")
-    public TResult deleteGroup(@PathVariable Long appGroupId) {
+    public TResult deleteGroup(@PathVariable Long appGroupId){
         Long userId = (Long) session.getAttribute(Constants.SESSION_KEY_USER_ID);
-        if (!appGroupService.delete(new EntityWrapper<AppGroup>().eq("app_group_id", appGroupId).eq("user_id", userId)))
+        if(!appGroupService.delete(new EntityWrapper<AppGroup>().eq("app_group_id",appGroupId).eq("user_id",userId)))
             return TResult.failure(TResultCode.BUSINESS_ERROR);
         return TResult.success();
     }
