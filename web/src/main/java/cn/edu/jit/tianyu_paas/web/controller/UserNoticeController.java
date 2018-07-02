1aeeebdb817a50128fa6d7e2c8168bb8d5a2a17c
nilongkang <1104475626@qq.com>
2018-07-03 00:09:43 +0800

    修改公告部分接口

--- web/src/main/java/cn/edu/jit/tianyu_paas/web/controller/UserNoticeController.java
+++ web/src/main/java/cn/edu/jit/tianyu_paas/web/controller/UserNoticeController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 /**
  * @author 倪龙康
@@ -51,7 +52,13 @@ public class UserNoticeController {
                 , new EntityWrapper<Notice>().orderBy("gmt_create", false));
         if (noticePage == null)
             return TResult.failure(TResultCode.FAILURE);
-        return TResult.success(noticePage);
+        List<Notice> notices = noticePage.getRecords();
+        Long userId = (Long) session.getAttribute(Constants.SESSION_KEY_USER_ID);
+        for (Notice notice : notices){
+            UserNotice userNotice = userNoticeService.selectOne(new EntityWrapper<UserNotice>().eq("user_id",userId).eq("notice_id",notice.getNoticeId()));
+            notice.setUserNotice(userNotice);
+        }
+        return TResult.success(notices);
     }
 
     /**
@@ -73,6 +80,7 @@ public class UserNoticeController {
         userNotice.setStatus(1);
         if (!userNoticeService.update(userNotice, new EntityWrapper<UserNotice>().eq("user_id", userId).eq("notice_id", noticeId)))
             return TResult.failure(TResultCode.FAILURE);
+        notice.setUserNotice(userNotice);
         return TResult.success(notice);
     }
 }
