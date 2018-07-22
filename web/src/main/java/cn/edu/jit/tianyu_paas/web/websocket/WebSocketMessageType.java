f2b325fd50904f6779ebfcea583c24ef09e29dbc
tianyu <tianyu@tianyu-studio.cn>
2018-07-22 09:42:52 +0800

    update

--- web/src/main/java/cn/edu/jit/tianyu_paas/web/websocket/WebSocketMessageType.java
+++ web/src/main/java/cn/edu/jit/tianyu_paas/web/websocket/WebSocketMessageType.java
@@ -1,7 +1,23 @@
 package cn.edu.jit.tianyu_paas.web.websocket;
 
+/**
+ * 前后台websocket通信的协议类型
+ */
 public enum WebSocketMessageType {
+    /**
+     * 公告
+     */
     NOTICE,
+    /**
+     * 消息（管理员推送消息给用户）
+     */
     MESSAGE,
-    BUILD_APPLICATION;
+    /**
+     * 应用构建信息
+     */
+    BUILD_APPLICATION,
+    /**
+     * 构建应用的结果
+     */
+    BUILD_APPLICATION_RESULT
 }
